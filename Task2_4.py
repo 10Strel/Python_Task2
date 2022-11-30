f7248f4ac570857723a59dfd4322da9ce03338bd
10Strel <manowartenarrows@gmail.com>
2022-11-30 22:00:53 +0300

    add Task2_4

---
+++ Task2_4.py
@@ -0,0 +1,39 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции вводятся с клавиатуры.
+
+from random import randint
+
+N = int(input('Введите размерность списка: '))
+
+numbers = []
+for i in range(-N, N + 1):
+    numbers.append(i)
+print(numbers)
+
+lst = []
+flag = False
+pos = 0
+
+while not flag:
+    pos = input('Введите позицию (для выхода нажмите q): ')
+
+    if pos == 'q':
+        flag = True
+        break
+
+    if (not pos.isdigit()):
+       continue
+
+    int_pos = int(pos)
+
+    if (int_pos not in range(0, len(numbers) + 1) or (int_pos in lst)):
+        continue
+
+    lst.append(int_pos)
+
+print(lst)
+
+res = 1
+for i in lst:
+    res *= numbers[i]
+
+print(res)
